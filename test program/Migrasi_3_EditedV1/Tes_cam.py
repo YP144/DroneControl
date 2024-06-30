b8fef7103e8f222f750821edc25481a7a3cc2346
YP144 <anakpintardapatnilai100@gmail.com>
2024-06-30 15:42:01 +0700

    add Integrasi.ipynb

--- test program/Migrasi_3_EditedV1/Tes_cam.py
+++ test program/Migrasi_3_EditedV1/Tes_cam.py
@@ -2,25 +2,20 @@ import cv2 as cv
 import time
 ardrone_url = 'tcp://192.168.1.1:5555'
 camera = cv.VideoCapture(ardrone_url)
-width = camera.get(3)
-height = camera.get(4)
-print(width, height)
+# width = camera.get(3)
+# height = camera.get(4)
+# print(width, height)
 # test = camera.get(cv.CAP_PROP_BUFFERSIZE)
 # print(test)
 while True:
-
     ret, frame = camera.read()
-    # sky = frame[0:200, 240:400]
     if ret:
         # fps = camera.get(cv.CAP_PROP_FPS)
         # print(fps)
-        # cv.imshow('frame', sky)
         cv.imshow('test', frame)
     elif ret == False:
         print("tidak ada gambar tertangkap")
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
-
-
 camera.release()
 cv.destroyAllWindows()
