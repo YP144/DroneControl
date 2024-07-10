f064013dfde16c72d08c4cd1f1fac1e58aac76be
YP144 <96116689+YP144@users.noreply.github.com>
2024-07-10 18:53:05 +0700

    ngolah data karakteristik 023863

--- matlab/testSim.m
+++ matlab/testSim.m
@@ -1,4 +1,25 @@
-sys = tf([37.05446],[1.486946 1 0 0])
-figure(1)
-plot(step(sys))
-%xlim([0 10])
+% Definisikan Parameter Sistem
+tau = 0.983; % Konstanta waktu sistem 
+K = 80.147  ; % Penguatan sistem
+A = 1; % Kemiringan sinyal ramp
+
+% Definisikan Sistem di Domain Laplace
+s = tf('s'); % Definisikan operator Laplace
+G = K / (tau * s + 1); % Fungsi transfer sistem
+
+% Definisikan Sinyal Ramp
+U = A / s^2; % Fungsi transfer sinyal ramp
+
+% Hitung Respon Sistem
+Y = G * U; % Fungsi transfer output
+
+% Simulasi dan Plot Respon
+t = 0:0.01:4; % Rentang waktu simulasi
+[y, t] = impulse(Y, t); % Hitung respon terhadap sinyal ramp
+
+% Plot hasil simulasi
+plot(t, y)
+title('Respon Sistem Orde 1 terhadap Sinyal Ramp')
+xlabel('Waktu (detik)')
+ylabel('Output y(t)')
+grid on
